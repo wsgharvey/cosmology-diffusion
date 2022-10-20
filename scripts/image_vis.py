14c640c9b9dcc628f65c8c032278428be1bcda91
William Harvey <wsgh@plai11.cs.ubc.ca>
2022-10-19 19:52:25 -0700

    bug fixes

--- scripts/image_vis.py
+++ scripts/image_vis.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     with open(model_config_path, "r") as f:
         model_args = argparse.Namespace(**json.load(f))
 
-    overdensities = samples.squeeze(dim=1)
+    max_density = 5.5
+    overdensities = samples.squeeze(axis=1)
     densities = np.exp(1 + overdensities) - 1  # densities in range [0, inf]
 
     model_kwargses = [pickle.load(open(kwargs_fname(f), 'rb')) for f in files]
@@ -37,16 +38,19 @@ if __name__ == "__main__":
 
     for sample_i, (sample, model_kwargs) in enumerate(zip(overdensities, model_kwargses)):
         D =  sample.shape[-1]
-        indices = [0, 1, 2, 3, 4] + [D, D+1, D+2, D+3, D+4]
+        indices = [0, 1, 2, 3, 4] + [D//2, D//2+1, D//2+2, D//2+3, D//2+4]
         fig, axes = plt.subplots(ncols=D, figsize=(D*2, 2))
         axes[0].set_ylabel(model_kwargs['y'].item())
         for ind, ax in zip(indices, axes):
             im = ax.imshow(sample[ind], cmap='gray')
             ax.set_title(str(ind))
             ax.axis('off')
-            ax.set_ylim(-1, 5)
+            ax.set_ylim(-1, max_density)
 
         fig.subplots_adjust(right=0.8)
         cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
         fig.colorbar(im, cax=cbar_ax)
-        fig.savefig(f"sample_{sample_i}.png")
+
+        save_dir = Path(args.eval_dir) / "vis"
+        save_dir.mkdir(parents=True, exist_ok=True) 
+        fig.savefig(save_dir / f"sample_{sample_i}.pdf", bbox_inches='tight')
